bdaacda152f1c6c8605b1d56c994927e3f402e48
kisa <sseguku@gmail.com>
2017-03-19 03:31:46 +0300

    ex1 and ex2

---
+++ ex1.rb
@@ -0,0 +1,7 @@
+puts "Hello, Kisakye"
+puts "Hello, Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "Said" do no touch this.'
---
+++ ex1_drill_1.rb
@@ -0,0 +1,8 @@
+puts "Hello, Kisakye"
+puts "Hello, Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "Said" do no touch this.'
+puts "Kisakye, is printing an extra line. Oh yah! This is fun :)"
